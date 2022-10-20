a96383ea98c9105431b53201bf2a6d316ed293b1
Ryan Taban <rat2@sfu.ca>
2022-10-20 00:42:27 -0700

    Implemented window in main

--- GalacticGreaseMonkey/src/main/java/Game/Main.java
+++ GalacticGreaseMonkey/src/main/java/Game/Main.java
@@ -1,8 +1,16 @@
 package Game;
 
+import javax.swing.JFrame;
 public class Main {
-    public static void main(String args[])  //static method
+    public static void main(String[] args)  //static method
     {
-        System.out.println("HelloWorld");
+        //System.out.println("HelloWorld");
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("Galactic Grease Monkey");
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
     }
 }
