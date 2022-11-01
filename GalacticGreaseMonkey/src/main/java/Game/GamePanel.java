575a711477ca9e38bae9a6d968d7c8e693188f8d
xgill15x <jason_gill15@hotmail.com>
2022-10-31 20:31:49 -0700

    tile size is now 24x24, scale changed from 3 to 2

--- GalacticGreaseMonkey/src/main/java/Game/GamePanel.java
+++ GalacticGreaseMonkey/src/main/java/Game/GamePanel.java
@@ -13,8 +13,8 @@ import java.awt.Dimension;
 public class GamePanel extends JPanel implements Runnable {
 
     // Screen Settings
-    final int originalTileSize = 16; // 16x16 tile
-    final int scale = 3;
+    final int originalTileSize = 24; // 16x16 tile
+    final int scale = 2;
 
     public final int tileSize = originalTileSize * scale; // 48x48
     public final int maxScreenCol = 24;
