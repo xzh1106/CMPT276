bca117ea164d8bfd3bc77c94651acfdef8357245
xgill15x <jason_gill15@hotmail.com>
2022-11-03 18:27:59 -0700

    added second spaceship item, altered map slightly

--- GalacticGreaseMonkey/src/main/java/Game/AssetSetter.java
+++ GalacticGreaseMonkey/src/main/java/Game/AssetSetter.java
@@ -14,6 +14,10 @@ public class AssetSetter {
         gp.spaceshipPart[0].worldX = 7 * gp.tileSize; // Set location for obj on map
         gp.spaceshipPart[0].worldY = 10 * gp.tileSize;
 
+        gp.spaceshipPart[1] = new OBJ_SpaceshipPart(gp); // Make a key object and save into obj array
+        gp.spaceshipPart[1].worldX = 20 * gp.tileSize; // Set location for obj on map
+        gp.spaceshipPart[1].worldY = 1 * gp.tileSize;
+
     }
 
     public void setDiamond() {
