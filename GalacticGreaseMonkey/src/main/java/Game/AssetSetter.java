8fc12451c7f6bb8aaa843bb4010ecdbeaae37b45
luan_nguyen_3@sfu.ca <57047641+luan30092000@users.noreply.github.com>
2022-11-04 22:37:47 -0700

    Allocate door in map

--- GalacticGreaseMonkey/src/main/java/Game/AssetSetter.java
+++ GalacticGreaseMonkey/src/main/java/Game/AssetSetter.java
@@ -18,6 +18,16 @@ public class AssetSetter {
         gp.spaceshipPart[1].worldX = 20 * gp.tileSize; // Set location for obj on map
         gp.spaceshipPart[1].worldY = 1 * gp.tileSize;
 
+
+        //Testing
+//        gp.spaceshipPart[0] = new OBJ_SpaceshipPart(gp); // Make a key object and save into obj array
+//        gp.spaceshipPart[0].worldX = 7 * gp.tileSize; // Set location for obj on map
+//        gp.spaceshipPart[0].worldY = 5 * gp.tileSize;
+//
+//        gp.spaceshipPart[1] = new OBJ_SpaceshipPart(gp); // Make a key object and save into obj array
+//        gp.spaceshipPart[1].worldX = 10 * gp.tileSize; // Set location for obj on map
+//        gp.spaceshipPart[1].worldY = 1 * gp.tileSize;
+
     }
 
     public void setDiamond() {
@@ -48,6 +58,12 @@ public class AssetSetter {
 
     }
 
+    public void setClosedDoor() {
+        gp.closedDoor[0] = new OBJ_ClosedDoor(gp);
+        gp.closedDoor[0].worldX = 30 * gp.tileSize;
+        gp.closedDoor[0].worldY = 10 * gp.tileSize;
+    }
+
     public void setAlien() {
         gp.alien[0] = new Alien(gp); // Make a key object and save into obj array
         gp.alien[0].worldX = 20 * gp.tileSize; // Set location for obj on map
@@ -71,4 +87,5 @@ public class AssetSetter {
         gp.alien[index].worldX = 30 * gp.tileSize; // Set location for obj on map
         gp.alien[index].worldY = 14 * gp.tileSize;
     }
+
 }
